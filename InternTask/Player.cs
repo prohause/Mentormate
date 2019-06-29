233f1fee8efa6f2a4d62133a2b83115dbd761e5a
prohause@abv.bg <Sevdjan8092>
2019-06-29 08:19:22 +0300

    Finished

---
+++ InternTask/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace InternTask
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public int PlayingSince { get; set; }
+
+        public string Position { get; set; }
+
+        public int Rating { get; set; }
+    }
+
+    //"Name": "Giannis Antetokounmpo",
+    //"PlayingSince": 2012,
+    //"Position": "SF",
+    //"Rating": 96
+}
---
+++ InternTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace InternTask
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var pathToInputFile = Console.ReadLine();
+            var maxYearsPlayed = int.Parse(Console.ReadLine());
+            var minPlayerRating = int.Parse(Console.ReadLine());
+            var pathToOutputFile = Console.ReadLine();
+
+            var inputString = File.ReadAllText(pathToInputFile);
+
+            var allPlayersParsed = JsonConvert.DeserializeObject<List<Player>>(inputString);
+
+            var lastQualifyingYear = DateTime.UtcNow.Year - maxYearsPlayed;
+
+            var qualifiedPlayers = allPlayersParsed.Where(p => p.PlayingSince >= lastQualifyingYear && p.Rating >= minPlayerRating).Select(x => new
+            {
+                x.Name,
+                x.Rating
+            }).OrderByDescending(x => x.Rating).ToList();
+
+            var output = new StringBuilder();
+            output.AppendLine("Name,Rating");
+
+            qualifiedPlayers.ForEach(p => output.AppendLine($"{p.Name}, {p.Rating}"));
+
+            File.WriteAllText(pathToOutputFile, output.ToString().TrimEnd());
+        }
+    }
+}
